d6149cbbf3cbcde86f834e123bedea3f52eff5c3
Gertjan De Mulder <gertjan.demuder@ugent.be>
2025-01-20 13:09:14 +0100

    Fix: HTTP 429 Error → Updated documentloader with cache

--- src/documentLoader.ts
+++ src/documentLoader.ts
@@ -1,70 +1,98 @@
-
 import {CONTEXTS} from "./contexts";
 import {logv2} from "./utils/log";
 import {IRegistry} from "./interfaces";
 
 interface CreateDocumentLoaderOptionsDefault {
-    logging: { urls: { missing: boolean; present: boolean }; documents: boolean; loadedContextNames: boolean };
+  logging: { urls: { missing: boolean; present: boolean }; documents: boolean; loadedContextNames: boolean };
 }
 
 export const createDocumentLoaderOptionsDefault: CreateDocumentLoaderOptionsDefault = {
-    logging: {
-        loadedContextNames: true,
-        urls: {
-            present:false, // Log URLs present in contexts
-            missing: true}, // Log URLs not present in contexts
-        documents: false // Log resolved document
-    }
+  logging: {
+    loadedContextNames: true,
+    urls: {
+      present: false, // Log URLs present in contexts
+      missing: true
+    }, // Log URLs not present in contexts
+    documents: false // Log resolved document
+  }
 }
 
+const fetchJsonDocument = async (url: string) => {
+  const response = await fetch(url, {headers: {Accept: 'application/ld+json'}})
+  let document = undefined
+  switch (response.headers.get('content-type')) {
+    case 'application/json':
+    case 'application/ld+json':
+    case 'application/ld+did+json':
+      document = await response.json()
+      break;
+    default:
+      throw new Error(
+        `Error: ${response.headers.get('content-type')} not yet supported.
+                        Status: ${response.status} - ${response.statusText}
+                        URL: ${response.url}
+                        `
+      )
+  }
+  return document
+}
 export const createDocumentLoader = (
-    contexts: any,
-    registry?: IRegistry
-    ) => {
+  contexts: any,
+  registry?: IRegistry
+) => {
 
-    const options = createDocumentLoaderOptionsDefault;
+  const options = createDocumentLoaderOptionsDefault;
+  const cache = new Map<string, any>()
 
-    if(options.logging.loadedContextNames)
-        console.log('Loaded contexts: ', Object.keys(contexts))
+  if (options.logging.loadedContextNames)
+    console.log('Loaded contexts: ', Object.keys(contexts))
 
-    return async (url:string) => {
-        if(url === undefined)
-            throw new Error('URL is undefined!')
-        if (url in contexts) {
-            if(options.logging.urls.present)
-                console.log(`URL: ${url}`)
-            if(options.logging.documents)
-                logv2(contexts[url], `📄Document:`)
+  return async (url: string) => {
+    if (url === undefined)
+      throw new Error('URL is undefined!')
 
-            return {
-                contextUrl: undefined, // this is for a context via a link header
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-                document: contexts[url], // this is the actual document that was loaded
-                documentUrl: url, // this is the actual context URL after redirects
-            }
-        }
+    // Check loaded contexts
+    if (url in contexts) {
+      if (options.logging.urls.present)
+        console.log(`URL: ${url}`)
+      if (options.logging.documents)
+        logv2(contexts[url], `📄Document:`)
 
-        if(registry!!) {
-
-            const resolvedDocument = registry!.resolve(url)
-            return {
-                contextUrl: undefined,
-                document: resolvedDocument,
-                documentUrl: url,
-            }
-        }
-
-        // No document to be loaded.
-        if(options.logging.urls.missing)
-            console.log(`⚠️Missing URL: ${url}`)
-
-        // return empty document if `url` is not in local contexts
-        return {
-            contextUrl: undefined,
-            documentUrl: url,
-            document: {},
-        }
+      return {
+        contextUrl: undefined, // this is for a context via a link header
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        document: contexts[url], // this is the actual document that was loaded
+        documentUrl: url, // this is the actual context URL after redirects
+      }
     }
+
+    if (registry!!) {
+      const resolvedDocument = registry!.resolve(url)
+      return {
+        contextUrl: undefined,
+        document: resolvedDocument,
+        documentUrl: url,
+      }
+    }
+
+    // If present, return from cache
+    if (cache.has(url)) {
+      return {
+        contextUrl: undefined,
+        documentUrl: url,
+        document: cache.get(url)
+      }
+    }
+
+    // Lastly, try to resolve from the network & cache
+    const document = await fetchJsonDocument(url)
+    cache.set(url, document)
+    return {
+      contextUrl: undefined,
+      documentUrl: url,
+      document
+    }
+  }
 }
 export const localDocumentLoader = createDocumentLoader(CONTEXTS)
 
